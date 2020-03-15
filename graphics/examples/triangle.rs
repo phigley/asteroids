c72b0891834dcde08ad06fecccd572099a0a633f
Peter Higley <phigley@gmail.com>
2020-03-15 14:45:29 -0700

    Simplifying triangle example

--- graphics/examples/triangle.rs
+++ graphics/examples/triangle.rs
@@ -6,24 +6,6 @@ struct App {
     model: model::Model,
 }
 
-impl App {
-    fn new(runner: &mut screen::ScreenRunner) -> Self {
-        let translation: Similarity2<f32> = Similarity2::identity();
-
-        let indices = [0, 1, 2];
-        let verts = [
-            Point2::new(-0.5, 0.5),
-            Point2::new(0.5, 0.5),
-            Point2::new(0.0, -0.5),
-        ];
-        let shape = runner.screen.create_shape(&verts, &indices, "triangle");
-        let yellow = color::Color::new(1.0, 1.0, 0.0, 1.0);
-        Self {
-            model: model::Model::new(shape, yellow, translation),
-        }
-    }
-}
-
 impl screen::ScreenCallbacks for App {
     fn render(&self, mut screen_render: screen::ScreenRender) {
         screen_render.draw_model(&self.model);
@@ -38,6 +20,18 @@ fn main() {
         Ok(created_screen) => created_screen,
     };
 
-    let app = App::new(&mut runner);
-    runner.run(app);
+    let translation: Similarity2<f32> = Similarity2::identity();
+
+    let indices = [0, 1, 2];
+    let verts = [
+        Point2::new(-0.5, 0.5),
+        Point2::new(0.5, 0.5),
+        Point2::new(0.0, -0.5),
+    ];
+    let shape = runner.screen.create_shape(&verts, &indices, "triangle");
+    let yellow = color::Color::new(1.0, 1.0, 0.0, 1.0);
+
+    let model = model::Model::new(shape, yellow, translation);
+
+    runner.run(App { model });
 }
